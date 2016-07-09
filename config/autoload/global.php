ba5ba9a007fe5207285159d8448562816d044d4d
Raphael Iarussi <pigaov10@gmail.com>
2016-07-09 14:49:26 -0300

    auth

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -15,8 +15,8 @@
  		'db' => array(
  				'driver'         => 'Pdo',
  				'dsn'            => 'mysql:dbname=tocadasnovinhas;host=localhost',
-        //'username' => 'root',
-        //'password' => '29367253',
+        'username' => 'root',
+        'password' => '29367253',
  				'driver_options' => array(
  						PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
  				),
