f1f6d3672bfe4c55d29e2235f3382088660f14e3
Raphael Iarussi <pigaov10@gmail.com>
2016-07-10 00:39:42 -0300

    çategoria

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -8,27 +8,36 @@ use Zend\View\Model\ViewModel;
 class IndexController extends AbstractActionController
 {
 
-    protected $videoTable;
+    protected $videoTable = null;
 
-  	public function getVideoTable(){
-  		if(!$this->videoTable){
-  			$sm = $this->getServiceLocator();
-  			$this->videoTable = $sm->get('Application\Model\VideoTable');
-  		}
-  		return $this->videoTable;
-  	}
-  	public function indexAction(){
-  		return new ViewModel(array(
-  				'data' => $this->getVideoTable()->fetchAll(),
-  		));
-  	}
+    public function getVideoTable()
+    {
+        if(!$this->videoTable){
+          			$sm = $this->getServiceLocator();
+          			$this->videoTable = $sm->get('Application\Model\VideoTable');
+          		}
+          		return $this->videoTable;
+    }
+
+    public function indexAction()
+    {
+        return new ViewModel(array(
+          				'data' => $this->getVideoTable()->fetchAll(),
+          		));
+    }
 
     public function detailAction()
     {
         return new ViewModel(array(
-          'data'=> $this->getVideoTable()->fetchById($this->params()->fromRoute('id'))
-        ));
+                  'data'=> $this->getVideoTable()->fetchById($this->params()->fromRoute('id'))
+                ));
+    }
+
+    public function categoryAction()
+    {
+        return new ViewModel();
     }
 
 
 }
+
