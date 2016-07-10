57e3620b5ac34c122f47eaeadb0ba623a9a1fa98
Raphael Iarussi <pigaov10@gmail.com>
2016-07-10 13:30:57 -0300

    äss

--- module/Application/src/Application/Model/VideoTable.php
+++ module/Application/src/Application/Model/VideoTable.php
@@ -1,6 +1,9 @@
 <?php
 namespace Application\Model;
 use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\Sql\Select;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Paginator\Paginator;
 
 class VideoTable
 {
@@ -9,8 +12,26 @@ class VideoTable
     {
         $this->tableGateway = $tableGateway;
     }
-    public function fetchAll()
+    public function fetchAll($paginated=false)
     {
+        if ($paginated) {
+            // create a new Select object for the table album
+            $select = new Select('video');
+            // create a new result set based on the Album entity
+            $resultSetPrototype = new ResultSet();
+            $resultSetPrototype->setArrayObjectPrototype(new Video());
+            // create a new pagination adapter object
+            $paginatorAdapter = new DbSelect(
+                // our configured select object
+                $select,
+                // the adapter to run it against
+                $this->tableGateway->getAdapter(),
+                // the result set to hydrate
+                $resultSetPrototype
+            );
+            $paginator = new Paginator($paginatorAdapter);
+            return $paginator;
+        }
         $resultSet = $this->tableGateway->select();
         return $resultSet;
     }
