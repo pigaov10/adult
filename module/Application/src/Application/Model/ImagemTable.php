474d87922ab2fb7b68bc896d5f33e0d9cbdb883e
Raphael Iarussi <pigaov10@gmail.com>
2016-07-13 00:11:05 -0300

    imagens

---
+++ module/Application/src/Application/Model/ImagemTable.php
@@ -0,0 +1,21 @@
+<?php
+namespace Application\Model;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\Sql\Select;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Paginator\Paginator;
+
+class ImagemTable
+{
+    protected $tableGateway;
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+    public function fetchAll($paginated=false)
+    {
+      $resultSet = $this->tableGateway->select();
+      return $resultSet;
+    }
+}
