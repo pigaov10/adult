0191474cf5b6842fedd67808ec4eee75333428f2
Raphael Iarussi <pigaov10@gmail.com>
2016-07-12 23:58:56 -0300

    imagens

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -9,44 +9,63 @@ class IndexController extends AbstractActionController
 {
 
     protected $videoTable = null;
+    protected $imagemTable = null;
 
     public function getVideoTable()
     {
         if(!$this->videoTable){
-          			$sm = $this->getServiceLocator();
-          			$this->videoTable = $sm->get('Application\Model\VideoTable');
-          		}
-          		return $this->videoTable;
+                  			$sm = $this->getServiceLocator();
+                  			$this->videoTable = $sm->get('Application\Model\VideoTable');
+                  		}
+                  		return $this->videoTable;
+    }
+
+    public function getImagemTable()
+    {
+        if(!$this->imagemTable){
+                  			$sm = $this->getServiceLocator();
+                  			$this->imagemTable = $sm->get('Application\Model\ImagemTable');
+                  		}
+                  		return $this->imagemTable;
     }
 
     public function indexAction()
     {
-      // grab the paginator from the AlbumTable
-      $paginator = $this->getVideoTable()->fetchAll(true);
-      // set the current page to what has been passed in query string, or to 1 if none set
-      $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', 1));
-      // set the number of items per page to 10
-      $paginator->setItemCountPerPage(6);
+        // grab the paginator from the AlbumTable
+              $paginator = $this->getVideoTable()->fetchAll(true);
+              // set the current page to what has been passed in query string, or to 1 if none set
+              $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', 1));
+              // set the number of items per page to 10
+              $paginator->setItemCountPerPage(6);
 
-      return new ViewModel(array(
-          'paginator' => $paginator
-      ));
+              return new ViewModel(array(
+                  'paginator' => $paginator
+              ));
     }
 
     public function detailAction()
     {
         return new ViewModel(array(
-                  'data'=> $this->getVideoTable()->fetchById($this->params()->fromRoute('id')),
-                  'titulo' => $this->params()->fromRoute('id'),
-                  'relacionado' => $this->getVideoTable()->fetchRandomVideos()
-                ));
+                          'data'=> $this->getVideoTable()->fetchById($this->params()->fromRoute('id')),
+                          'titulo' => $this->params()->fromRoute('id'),
+                          'relacionado' => $this->getVideoTable()->fetchRandomVideos()
+                        ));
     }
 
     public function categoryAction()
     {
-      return new ViewModel(array(
-                'data'=> $this->getVideoTable()->fetchByCategory($this->params()->fromRoute('id')),
-                'titulo' => $this->params()->fromRoute('id')
-              ));
+        return new ViewModel(array(
+                        'data'=> $this->getVideoTable()->fetchByCategory($this->params()->fromRoute('id')),
+                        'titulo' => $this->params()->fromRoute('id')
+                      ));
     }
+
+    public function fotosAction()
+    {
+      return new ViewModel(array(
+                      'data'=> $this->getVideoTable()->getImagemTable()
+                    ));
+    }
+
+
 }
