dbfe9c4401a74fe014a0b0f6a58ee8b1d812a767
Raphael Iarussi <pigaov10@gmail.com>
2016-07-18 20:20:27 -0300

    pornolandia crawler

--- Crawler2.py
+++ Crawler2.py
@@ -7,7 +7,7 @@ from datetime import datetime
 default_header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0',}
 
 sites = [
-     'http://www.pornolandia.xxx/search?search=&search_query=loira&search_type=videos&o=mv',
+     'http://www.pornhub.com/video/search?search=loira',
     # 'http://www.hellxx.com/index.php?module=videos&tag=d',
     # 'http://www.redtube.com/?search=s',
     # 'http://pt.xhamster.com/search.php?from=&new=&q=asasaa&qcat=video',
@@ -18,6 +18,6 @@ content = urllib2.urlopen(sites[0]).read()
 
 soup = BeautifulSoup(content)
 
-for video in soup.find("div",{"class":"wellborder"}):
+for video in soup.find("div",{"class":"phimage"}):
     print video.find("a")
     print "----------"
